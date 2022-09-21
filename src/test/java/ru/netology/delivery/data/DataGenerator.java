92504aea0d2b60d121bda55b5364fc4ed6e21036
Екатерина Устинова <dadsda@inbox.ru>
2022-09-21 22:58:29 +0700

    add project

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,50 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    static Faker faker = new Faker ((new Locale("ru")));
+
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        return faker.address().city();
+
+    }
+    public static String generateName(String locale) {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        return faker.phoneNumber().cellPhone();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(
+                    generateName(locale),
+                    generatePhone(locale),
+                    generateCity(locale)
+            );
+        }
+    }
+    @Value
+    public static class UserInfo {
+        String name;
+        String phone;
+        String city;
+    }
+}
